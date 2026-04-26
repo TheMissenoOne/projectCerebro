71f9e0a4df7285311d1445c7a20afb1782b3198b
Dev <dev@xmen-ttrpg.com>
2026-04-26 16:48:33 -0300

    fix: GM whitelist env var injection

--- assets/js/gm-config.js
+++ assets/js/gm-config.js
@@ -1,5 +1,7 @@
 // GM whitelist - users with these emails will be treated as Game Masters
-// Injected at build time via GitHub Actions from environment variables
-const GM_WHITELIST = '{{VITE_GM_WHITELIST}}'
-  ? '{{VITE_GM_WHITELIST}}'.split(',').filter(Boolean)
-  : [];
+// Injected at build time via {{VITE_GM_WHITELIST}}
+(function() {
+  var whitelist = '{{VITE_GM_WHITELIST}}'.split(',').filter(Boolean);
+  window.GM_WHITELIST = whitelist;
+  console.log('[gm-config.js] GM whitelist loaded:', whitelist);
+})();
