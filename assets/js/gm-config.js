155ae5c19daa6a2d80ebae59372bb9ccba427b36
Dev <dev@xmen-ttrpg.com>
2026-04-26 16:47:34 -0300

    fix: inject GitHub env vars at build time via sed

--- assets/js/gm-config.js
+++ assets/js/gm-config.js
@@ -1,5 +1,5 @@
 // GM whitelist - users with these emails will be treated as Game Masters
-// Set VITE_GM_WHITELIST in .env file (not committed)
-const GM_WHITELIST = (window.__env && window.__env.VITE_GM_WHITELIST) 
-  ? window.__env.VITE_GM_WHITELIST.split(',').filter(Boolean)
+// Injected at build time via GitHub Actions from environment variables
+const GM_WHITELIST = '{{VITE_GM_WHITELIST}}'
+  ? '{{VITE_GM_WHITELIST}}'.split(',').filter(Boolean)
   : [];
